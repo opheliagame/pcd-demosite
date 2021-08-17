8c00b80e40e6daa54d10f8304ff1e634b8f9dbcd
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2021-08-17 23:15:15 +0530

    simple gravity button design plus gravity on all pages

--- src/static/js/gravitymatter.js
+++ src/static/js/gravitymatter.js
@@ -10,38 +10,41 @@ var Engine = Matter.Engine,
 const bodies = {}
 // create two boxes and a ground
 const div = document.querySelectorAll('.matter')
-const boxes = Array.from(div).map(d => {
+const boxes = Array.from(div).map((node, index) => {
+  
   let body = {
-    'x': d.offsetLeft,
-    'y': d.offsetTop,
-    'width': d.offsetWidth,
-    'height': d.offsetHeight
+    'x': node.offsetLeft,
+    'y': node.offsetTop,
+    'width': node.offsetWidth,
+    'height': node.offsetHeight
   }
   const static = Math.random() < 0.5 ? true : false
   const angle = Math.floor(Math.random()*Math.PI*2 - Math.PI)
-  bodies[d.id] = body
+  node.id = `body-${index}`
+  bodies[node.id] = body
   
-  return Bodies.rectangle(body.x, body.y, body.width, body.height, {id: d.id, angle: angle})
+  return Bodies.rectangle(body.x, body.y, body.width, body.height, {id: node.id})
 })
-var ground = Bodies.rectangle(window.innerWidth/2, window.innerHeight-40,  window.innerWidth*2, 10, { isStatic: true });
+var ground = Bodies.rectangle(window.innerWidth/2, window.innerHeight-20,  window.innerWidth*2, 10, { isStatic: true });
 
 const turnOnGravity = (state) => {
   if(state) {
     var engine = Engine.create();
     // var render = Render.create({
-    //   element: document.body,
+    //   canvas: document.getElementById('canvas'),
     //   engine: engine
     // });
   
     Composite.add(engine.world, boxes);
     Composite.add(engine.world, ground);
   
+    // Render.run(render)
     var runner = Runner.create();
     Runner.run(runner, engine);
   
     Events.on(runner, "afterUpdate", () => {
       boxes.forEach(box => {
-        if(!box.isStatic) {
+        
           const div = document.getElementById(box.id)
           const prevPos = box.positionPrev
           const pos = box.position
@@ -51,10 +54,10 @@ const turnOnGravity = (state) => {
           div.style.position = 'absolute'
           div.style.left = `${pos.x}px`
           div.style.top = `${pos.y}px`
-          div.style.transform = `rotate(${rot}deg)`
+          div.style.transform = `rotate(${rot}rad)`
           div.style.width = `${bodies[box.id].width}px`
           div.style.height = `${bodies[box.id].height}px`
-        } 
+        
       })
     })
   }
