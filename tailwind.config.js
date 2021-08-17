c560b6351e1cb1477c8314ba36a567212c92852a
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2021-08-18 00:21:27 +0530

    sample deploy version#1 with main

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,20 @@ module.exports = {
   purge: [ '_site/**/*.html' ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontSize: {
+      'xxs': '.50rem',
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    },
     extend: {
       fontFamily: {
         sans: ["'Poppins'", 'sans-serif']
