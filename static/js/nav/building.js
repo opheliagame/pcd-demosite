68e337e6f82857257492321ef3ad2d61e440653c
opheliagame <opheliagame@users.noreply.github.com>
2021-09-17 18:59:18 +0000

    deploy: 4ef6684b2fff0b7800c71239213270f8d0e39590

--- static/js/nav/building.js
+++ static/js/nav/building.js
@@ -4,48 +4,120 @@ class Building {
     this.showModal = false
     this.prevRoom = null
     this.grid = json
-    this.rows = this.grid[0].length
-    this.cols = Object.keys(this.grid).length
-    this.edge = width/this.rows
+    this.rows = this.grid.rows
+    this.cols = this.grid.cols
+    this.edge = width/this.cols
     this.sprite = sprite
+    this.back = createGraphics((this.cols)*this.edge, (this.rows)*this.edge)
   }
 
   build() {
     
-    for(let i = 0; i < this.rows; i++) {
-      for(let j = 0; j < this.cols; j++) {
-        if (this.grid[j][i] === 1) {
-          let door = new Door(i*this.edge, j*this.edge, this.edge, this.sprite)
-          this.add(door)
-        }
+    for(let i = 0; i < this.rows*this.cols; i++) {
+      let block = this.grid.blocks[i]
+      if (block.filled === true) {
+        let door = new Door(block.x*this.edge, block.y*this.edge, this.edge, this.sprite)
+        this.add(door)
       }
     }
    
     this.doors.forEach(door => {
-      fetch(`https://source.unsplash.com/featured/?pixel`).then((response) => {   
+      // fetch().then((response) => {   
         let div = document.createElement('div');
         div.id = door.roomID
         div.classList.add('hidden', 'absolute', 'flex', 'w-screen', 'h-screen', 'items-center', 'justify-center');
         div.innerHTML = `
         <div class="w-1/2 max-h-full overflow-scroll border-4 border-black bg-yellow-300 p-4">
         <p>${div.id}</p>
-        <img class="image" src="${response.url}" />
+        <img class="image" src="" />
         </div>`     
         modal.appendChild(div);
         
         door.room = document.getElementById(door.roomID)
-      }) 
+      // }) 
     })
-    console.log(this.doors)
-    
+    console.log(this)
   }
 
-  draw() {
-    this.doors.forEach(d => d.draw())
+  draw(camera) {
+    push()
+    translate(-camera.x, -camera.y)
+    image(this.back, 0, 0)
+    pop()
+    // this.doors.forEach(d => d.draw(camera))
+  }
+
+  drawIndoor(camera) {
+    
+    for(let i = 0; i < (this.rows-1)*(this.cols-1); i++) {
+      let block = this.grid.blocks[i]
+      let x = block.x*this.edge
+      let y = block.y*this.edge
+      let tile = this.sprite.get((block.tileIndex%8)*16, Math.floor(block.tileIndex/8)*16, 16, 16)
+      this.back.push()
+      this.back.translate(x, y)
+      this.back.image(tile, 0, 0, this.edge, this.edge)
+      this.back.pop()
+
+      // push()
+      // translate(x+res*0.5, y+res*0.5)
+      // textSize(res*0.5)
+      // text(state, 0, 0)
+      // pop()
+      
+      
+    }
+  }
+
+  drawOnce(camera) {
+    let count = 0
+    for(let i = 0; i < this.rows; i++) {
+      for(let j = 0; j < this.cols; j++) {
+        if (this.grid[j][i] === 1) {
+          let door = this.doors[count]
+          door.draw(camera)
+          count += 1
+        }
+        else {
+          this.back.push()
+          this.back.translate(i*this.edge, j*this.edge)
+
+          // this is grass
+          let leftCell = (i-1) >= 0 ? this.grid[j][i-1] : undefined, 
+              rightCell = (i+1) < this.rows ? this.grid[j][i+1] : undefined, 
+              bottomCell = (j+1) < this.cols ? this.grid[j+1][i] : undefined, 
+               topCell = (j-1) >= 0 ? this.grid[j-1][i] : undefined
+          if([leftCell, rightCell, bottomCell, topCell].every(c => c === 0)) {            
+            this.back.image(this.sprite.get(15, 15, 15, 15), 0, 0, this.edge, this.edge)
+          }
+          else if(leftCell === undefined) {
+            this.back.image(this.sprite.get(0, 10, 25, 25), 0, 0, this.edge, this.edge)
+            if(topCell === undefined) {
+              this.back.image(this.sprite.get(0, 0, 25, 25), 0, 0, this.edge, this.edge)
+            }
+            else if(bottomCell === undefined) {
+              this.back.image(this.sprite.get(0, 21, 25, 25), 0, 0, this.edge, this.edge)
+            }
+          } 
+          else if(rightCell === undefined) {
+            this.back.image(this.sprite.get(20, 10, 25, 25), 0, 0, this.edge, this.edge)
+            if(topCell === undefined) {
+              this.back.image(this.sprite.get(20, 0, 25, 25), 0, 0, this.edge, this.edge)
+            }
+            else if(bottomCell === undefined) {
+              this.back.image(this.sprite.get(20, 21, 25, 25), 0, 0, this.edge, this.edge)
+            }
+          }
+          else if(topCell === undefined) {}
+
+          this.back.pop()
+        }
+      }
+    }
   }
 
   add(door) {
-    door.roomID = this.doors.length
+    door.roomID = `room${this.doors.length}`
     this.doors.push(door)
   }
 
@@ -56,12 +128,13 @@ class Building {
     // this.room.classList.toggle('hidden', !this.showModal) 
     if(currDoor.length !== 0) {
       this.room = currDoor[0].room
-      this.prevRoom = this.room
+      console.log(currDoor)
       this.room.classList.toggle('hidden', !this.showModal)
+      this.prevRoom = this.room
     } 
     if(this.prevRoom !== null) {
       this.prevRoom.classList.toggle('hidden', !this.showModal)
     }
-    // console.log(this.prevRoom)
+    
   }
 }
--- static/js/nav/sketch.js
+++ static/js/nav/sketch.js
@@ -3,47 +3,59 @@ let modal = document.getElementById('modal')
 let building, user
 let height = window.innerHeight
 let width = window.innerWidth
+let xMax, yMax
+let camera = new Camera(0, 0, width, height)
 let userSprite = []
 let farmSprite, floorImage
 let gridData
-let urlPrefix = '/pcd-demosite/static/assets/'
+let urlPrefix = '/static/assets/'
 
 function preload() {
-  gridData = loadJSON(urlPrefix + 'grid.json')
+  gridData = loadJSON(urlPrefix + 'pcd-town.json')
   let spriteBody = loadImage(urlPrefix + 'characters_free/assets/character_animation.png')
   let spriteHair = loadImage(urlPrefix + 'characters_free/assets/hair_braids_brown.png')
   let spritePants = loadImage(urlPrefix + 'characters_free/assets/pants_black.png')
   let spriteShirt = loadImage(urlPrefix + 'characters_free/assets/shirt_basic_red.png')
   let spriteShoes = loadImage(urlPrefix + 'characters_free/assets/shoes_brown.png')
   userSprite.push(spriteBody, spriteHair, spritePants, spriteShirt, spriteShoes) 
-  farmSprite = loadImage(urlPrefix + 'farm_free/free.png')
+  farmSprite = loadImage(urlPrefix + 'tilation-indoor.png')
 }
 
 function setup() {
-  createCanvas(width, height)
-  
   building = new Building(gridData, farmSprite)
+  xMax = building.cols * building.edge 
+  yMax = building.rows * building.edge
+  
+  createCanvas(width, height)
+
   let g = createGraphics(32*8, 32*2)
   userSprite.forEach(img => g.image(img, 0, 0))
   user = new User(0, 0, building.edge, g.get())
   building.build()
 
   g = createGraphics(width, height)
-  for(let i = 0; i < width; i+=building.edge) {
-    for(let j = 0; j < height; j+=building.edge) {
+  for(let i = 0; i < g.width; i+=building.edge) {
+    for(let j = 0; j < g.height; j+=building.edge) {
       g.image(farmSprite.get(0, 0, 48, 48), i, j, building.edge, building.edge)
     }
   } 
   floorImage = g.get()
+  building.drawIndoor(camera)
+
+  // frameRate(10)
+  console.log(building.grid.blocks)
 }
 
 function draw() {
-  // background(255)
-  // background('rgba(135,148,77, 0.5)')
   background(255)
-  image(floorImage, 0, 0)
   
-  building.draw()
-  user.io(user.w/10)
-  user.draw()  
+  building.draw(camera)
+  camera.move(user)
+  user.io(building, xMax, yMax)
+  user.draw(camera)  
 }
+
+// function keyPressed() {
+//   user.io(keyCode, building, xMax, yMax)
+// }
+
