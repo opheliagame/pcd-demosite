1ebfc60804b36bad11ef3a05b2a0eeb7d2ab986d
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2021-09-24 00:32:07 +0530

    new layout plus seats

--- src/static/js/nav/building.js
+++ src/static/js/nav/building.js
@@ -1,14 +1,14 @@
 class Building {
-  constructor(json, sprite, sprite2) {
+  constructor(gridJson, siteJson, sprite) {
     this.doors = new Array()
     this.showModal = false
     this.prevRoom = null
-    this.grid = json
+    this.grid = gridJson
+    this.embeds = siteJson
     this.rows = this.grid.rows
     this.cols = this.grid.cols
     this.edge = width/this.cols
     this.sprite = sprite
-    this.sprite2 = sprite2
     this.back = createGraphics((this.cols)*this.edge, (this.rows)*this.edge)
   }
 
@@ -16,26 +16,31 @@ class Building {
     
     for(let i = 0; i < this.rows*this.cols; i++) {
       let block = this.grid.blocks[i]
-      if (block.filled === true) {
+      if (block.type === 'seat') {
         let door = new Door(block.x*this.edge, block.y*this.edge, this.edge, this.sprite)
         this.add(door)
       }
     }
    
-    this.doors.forEach(door => {
-      // fetch().then((response) => {   
-        let div = document.createElement('div');
-        div.id = door.roomID
-        div.classList.add('hidden', 'absolute', 'flex', 'w-screen', 'h-screen', 'items-center', 'justify-center');
-        div.innerHTML = `
-        <div class="w-1/2 max-h-full overflow-scroll border-4 border-black bg-yellow-300 p-4">
-        <p>${div.id}</p>
-        <img class="image" src="" />
-        </div>`     
-        modal.appendChild(div);
-        
-        door.room = document.getElementById(door.roomID)
-      // }) 
+    this.doors.forEach((door, index) => {
+      let len = Object.keys(this.embeds).length
+      let embed = this.embeds[index % len]
+      let embedSrc = embed.src
+      let embedName = embed.name
+      let div = document.createElement('div');
+      div.id = door.roomID
+      div.classList.add('hidden', 'absolute', 'flex', 'w-screen', 'h-screen', 'items-center', 'justify-center');
+      div.innerHTML = 
+      `
+      <div class="w-1/2 h-full overflow-scroll border-4 border-black bg-white p-4">
+      <p class="bg-yellow-300 uppercase px-2">${embedName}</p>
+      <embed class="h-full w-full" src=${embedSrc} />
+      </div>
+      `     
+      modal.appendChild(div);
+      
+      door.room = document.getElementById(door.roomID)
+     
     })
     console.log(this)
   }
@@ -45,80 +50,22 @@ class Building {
     translate(-camera.x, -camera.y)
     image(this.back, 0, 0)
     pop()
-    // this.doors.forEach(d => d.draw(camera))
   }
 
-  drawIndoor(camera) {
+  drawBackground(camera) {
     
     for(let i = 0; i < (this.rows)*(this.cols); i++) {
       let block = this.grid.blocks[i]
-      let x = block.x*this.edge
-      let y = block.y*this.edge
-      let tile = this.sprite.get((block.tileIndex%8)*16, Math.floor(block.tileIndex/8)*16, 16, 16)
-      if(block.type === 'outside') {
-        let tx = block.tileIndex % 2
-        let ty = Math.floor(block.tileIndex / 2)
-        tile = this.sprite2.get(tx*16, ty*16, 16, 16)
-      }
+      let x = block.x * this.edge
+      let y = block.y * this.edge
+      let tx = block.tileIndex % 3
+      let ty = Math.floor(block.tileIndex / 3)
+      let tile = this.sprite.get(tx*16, ty*16, 16, 16)
       this.back.push()
       this.back.translate(x, y)
       this.back.image(tile, 0, 0, this.edge, this.edge)
       this.back.pop()
-
-      // push()
-      // translate(x+res*0.5, y+res*0.5)
-      // textSize(res*0.5)
-      // text(state, 0, 0)
-      // pop()
       
-      
-    }
-  }
-
-  drawOnce(camera) {
-    let count = 0
-    for(let i = 0; i < this.rows; i++) {
-      for(let j = 0; j < this.cols; j++) {
-        if (this.grid[j][i] === 1) {
-          let door = this.doors[count]
-          door.draw(camera)
-          count += 1
-        }
-        else {
-          this.back.push()
-          this.back.translate(i*this.edge, j*this.edge)
-
-          // this is grass
-          let leftCell = (i-1) >= 0 ? this.grid[j][i-1] : undefined, 
-              rightCell = (i+1) < this.rows ? this.grid[j][i+1] : undefined, 
-              bottomCell = (j+1) < this.cols ? this.grid[j+1][i] : undefined, 
-               topCell = (j-1) >= 0 ? this.grid[j-1][i] : undefined
-          if([leftCell, rightCell, bottomCell, topCell].every(c => c === 0)) {            
-            this.back.image(this.sprite.get(15, 15, 15, 15), 0, 0, this.edge, this.edge)
-          }
-          else if(leftCell === undefined) {
-            this.back.image(this.sprite.get(0, 10, 25, 25), 0, 0, this.edge, this.edge)
-            if(topCell === undefined) {
-              this.back.image(this.sprite.get(0, 0, 25, 25), 0, 0, this.edge, this.edge)
-            }
-            else if(bottomCell === undefined) {
-              this.back.image(this.sprite.get(0, 21, 25, 25), 0, 0, this.edge, this.edge)
-            }
-          } 
-          else if(rightCell === undefined) {
-            this.back.image(this.sprite.get(20, 10, 25, 25), 0, 0, this.edge, this.edge)
-            if(topCell === undefined) {
-              this.back.image(this.sprite.get(20, 0, 25, 25), 0, 0, this.edge, this.edge)
-            }
-            else if(bottomCell === undefined) {
-              this.back.image(this.sprite.get(20, 21, 25, 25), 0, 0, this.edge, this.edge)
-            }
-          }
-          else if(topCell === undefined) {}
-
-          this.back.pop()
-        }
-      }
     }
   }
 
